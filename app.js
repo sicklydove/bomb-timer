6dfedacd00aed3d0fb020ae12714cde6a85c5ae1
Laurie <laurence.james@gmail.com>
2015-12-14 20:44:34 +0000

    Hacky simple impl

--- app.js
+++ app.js
@@ -3,19 +3,49 @@ fs = require('fs');
  
 port = 3000;
 host = '127.0.0.1';
- 
+
+users = {}
+BOMB_SECS = 40
+
 server = http.createServer( function(req, res) {
  
     if (req.method == 'POST') {
-        console.log("Handling POST request...");
         res.writeHead(200, {'Content-Type': 'text/html'});
  
         var body = '';
         req.on('data', function (data) {
             body += data;
         });
+
         req.on('end', function () {
-            console.log("POST payload: " + body);
+            obj = JSON.parse(body)
+            var username = obj.player.name;
+            var ts = obj.provider.timestamp;
+
+            if(!users.hasOwnProperty(username)){
+                users[username] = {'planted': false, 'explodes': null};
+            }
+
+            player = users[username];
+
+            if(obj.hasOwnProperty('round')){
+
+                // bomb WAS NOT planted and now IS
+                if(obj.round.bomb == "planted"){
+                    if(!player.planted){
+                        player = {
+                            "planted": true,
+                            "explodes": ts + BOMB_SECS * 1000
+                        }
+                    }
+                }
+
+                // round over - reset bomb status
+                if(obj.round.phase == "over"){
+                    player = {'planted': false, 'explodes': null};
+                }
+            }
+
         	res.end( '' );
         });
     }
