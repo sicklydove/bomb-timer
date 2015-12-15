581e7aaa7b9fc24e82cecb3dccb6e943c08f2f4b
Laurie <laurence.james@gmail.com>
2015-12-15 00:09:38 +0000

    tidyup

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ http = require('http');
 fs = require('fs');
 url = require('url')
  
-port = 3000;
-
-users = {}
 BOMB_SECS = 40
+PORT = 3000;
 
+// Keep all users in in-memory cache
+// This'll work for now, but should be moved to mongo or something if popular
+users = {}
 server = http.createServer( function(req, res) {
  
+    // POST requests coming from CSGO gamestate
     if (req.method == 'POST') {
         res.writeHead(200, {'Content-Type': 'text/html'});
  
@@ -18,9 +20,10 @@ server = http.createServer( function(req, res) {
         });
 
         req.on('end', function () {
-            obj = JSON.parse(body)
-            var username = obj.player.name;
-            var ts = obj.provider.timestamp;
+            var gamestate = JSON.parse(body);
+
+            var username = gamestate.player.name;
+            var timestamp = gamestate.provider.timestamp;
 
             if(!users.hasOwnProperty(username)){
                 users[username] = {'planted': false, 'explodes': null};
@@ -28,40 +31,39 @@ server = http.createServer( function(req, res) {
 
             player = users[username];
 
-            if(obj.hasOwnProperty('round')){
-
-                // bomb WAS NOT planted and now IS
-                if(obj.round.bomb == "planted"){
-                    if(!player.planted){
-                        player = {
-                            "planted": true,
-                            "explodes": ts + BOMB_SECS * 1000
-                        }
+            if(gamestate.hasOwnProperty('round')){
+                if(gamestate.round.bomb == "planted" && !player.planted){
+                    player = {
+                        "planted": true,
+                        "explodes_at": timestamp + (BOMB_SECS * 1000)
                     }
                 }
 
-                // round over - reset bomb status
-                if(obj.round.phase == "over"){
+                if(gamestate.round.phase == "over"){
                     player = {'planted': false, 'explodes': null};
                 }
+
+                users[username] = player;
             }
 
-            users[username] = player;
-            console.log(users);
+            if(gamestate.hasOwnProperty('map') && gamestate.map.phase == "gameover"){
+                delete(users[username]);
+            }
 
         	res.end( '' );
         });
     }
-    else {
-        var url_parts = url.parse(req.url, true);
-        var qry = url_parts.query
 
-        console.log(JSON.stringify(users[qry.username]));
-        res.writeHead(200, {'Content-Type': 'json'});
+    // GET requests coming from client
+    else {
+        var qry = url.parse(req.url, true).query;
+
+        res.writeHead(200, {'Content-Type': 'application/json'});
+
         res.end(JSON.stringify(users[qry.username]));
     }
  
 });
  
-server.listen(port);
-console.log('Listening on port ' + port);
+server.listen(PORT);
+console.log('Listening on port ' + PORT);
